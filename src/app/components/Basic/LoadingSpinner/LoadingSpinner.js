e0b799b8e8059f90a3e288511a35d0534f05a8d2
Francesco Negri <francesconegri@gmail.com>
2016-06-03 12:55:55 +0200

    Split lines longer than 100 chars

--- src/app/components/Basic/LoadingSpinner/LoadingSpinner.js
+++ src/app/components/Basic/LoadingSpinner/LoadingSpinner.js
@@ -46,10 +46,17 @@ export default class LoadingSpinner extends React.Component {
   }
 
   getLocals() {
-    const { color: _color, overlayColor: _overlayColor, theme, message: _message, size } = this.props;
+    const {
+      color: _color,
+      overlayColor: _overlayColor,
+      theme,
+      message: _message,
+      size
+    } = this.props;
     const { visible } = this.state;
     const color = visible ? _color : 'transparent';
-    const overlayColor = _overlayColor || (theme === 'light') ? 'rgba(255,255,255,.5)' : 'rgba(128,128,128,.5)';
+    const overlayColor = _overlayColor ||
+      (theme === 'light') ? 'rgba(255,255,255,.5)' : 'rgba(128,128,128,.5)';
     const message = {
       content: _message,
       color: _message && color
--- src/app/components/Basic/loaders/fillFlexCenteredLoading.js
+++ src/app/components/Basic/loaders/fillFlexCenteredLoading.js
@@ -4,6 +4,11 @@ import { FlexView } from 'Basic';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 export default loading({
-  wrapper: <FlexView hAlignContent='center' vAlignContent='center' style={{ width: '100%', height: '100%', position: 'relative' }} />,
+  wrapper:
+    <FlexView
+      hAlignContent='center'
+      vAlignContent='center'
+      style={{ width: '100%', height: '100%', position: 'relative' }}
+    />,
   loader: <LoadingSpinner />
 });
