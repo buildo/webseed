28b1c7f339956d267d54a225f9a2dafccd4d806c
Francesco Negri <francesconegri@gmail.com>
2016-06-06 18:27:49 +0200

    Tutorial - Step 1

--- src/app/components/Hello/Hello.js
+++ src/app/components/Hello/Hello.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { props, t } from 'tcomb-react';
 import { pure } from 'revenge';
+import connect from 'state/connect';
 
 import './hello.scss';
 
 @pure
+@connect({ formal: t.maybe(t.Boolean) })
+@props({
+  transition: t.Function,
+  formal: t.maybe(t.Boolean)
+})
 export default class Hello extends React.Component {
+
+  toggle = () => {
+    this.props.transition({
+      formal: !this.props.formal
+    });
+  }
 
   render() {
     return (
       <div className='hello'>
-        <h1>Hello</h1>
+        <h1>
+          <a onClick={this.toggle}>{this.props.formal ? 'Good Morning' : 'Hello'}</a>
+        </h1>
       </div>
     );
   }
