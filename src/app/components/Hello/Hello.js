96e1029435f966a2ec69edc64eb24f7e1b6480cb
Francesco Negri <francesconegri@gmail.com>
2016-06-09 16:24:04 +0200

    Tutorial - Step 3

--- src/app/components/Hello/Hello.js
+++ src/app/components/Hello/Hello.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { props, t } from 'tcomb-react';
-import { pure } from 'revenge';
+import { pure, skinnable } from 'revenge';
 
 import './hello.scss';
 
 @pure
+@skinnable()
 @props({
   formal: t.Boolean,
   toggle: t.Function
 })
 export default class Hello extends React.Component {
 
-  render() {
+  formalGreeting() {
+    const hours = new Date().getHours();
+    if (hours > 20) return 'Good Night';
+    else if (hours > 13) return 'Good Afternoon';
+    else return 'Good Morning';
+  }
+
+  getLocals() {
+    const { toggle } = this.props;
+    const greeting = this.props.formal ? this.formalGreeting() : 'Hello';
+
+    return { toggle, greeting };
+  }
+
+  template({ toggle, greeting }) {
     return (
       <div className='hello'>
         <h1>
-          <a onClick={this.props.toggle}>{this.props.formal ? 'Good Morning' : 'Hello'}</a>
+          <a onClick={toggle}>{greeting}</a>
         </h1>
       </div>
     );
