f3a66c5c3b38ef1fff27ffddcfbd15b7d56330b8
Giovanni Gonzaga <giovanni@buildo.io>
2016-06-10 10:05:50 +0200

    Tutorial - Step 6

--- src/app/components/Hello/HelloContainer.js
+++ src/app/components/Hello/HelloContainer.js
@@ -1,15 +1,25 @@
+import React from 'react';
 import Hello from './Hello';
 import t from 'tcomb';
 import containerFactory from 'react-container';
 import allQueries from 'queries';
 import allCommands from 'commands';
+import loading from 'react-avenger/loading';
+import LoadingSpinner from 'buildo-react-components/src/loading-spinner';
+import 'buildo-react-components/src/loading-spinner/style.scss';
 
 const container = containerFactory({ allQueries, allCommands });
+
+const loadingDecorator = loading({
+  wrapper: <div style={{ textAlign: 'center', position: 'relative', minHeight: 100 }} />,
+  loader: <LoadingSpinner size='medium' />
+});
 
 const HelloContainer = container(Hello, {
   connect: { formal: t.maybe(t.Boolean) },
   queries: ['user'],
   commands: ['doRefreshUser'],
+  loadingDecorator,
   mapProps: ({ transition, formal = false, user, doRefreshUser }) => ({
     toggle: () => {
       transition({ formal: !formal });
