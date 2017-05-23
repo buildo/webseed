d90f4f33917eb0b1ffd348383a654d161edcd20c
Eric Camellini <eric.camellini@buildo.io>
2017-05-23 10:26:57 +0200

    Removing useless commented code

--- src/app/transitions.js
+++ src/app/transitions.js
@@ -1,11 +1,3 @@
 export function transitionReducer(state) {
-  /* if (shouldRedirectToLogin(state)) {
-    return toLogin()(state);
-  }
-
-  if (shouldRedirectToDefaultView(state)) {
-    return fromLogin()(state);
-  }*/
-
   return state;
 }
