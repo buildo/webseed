bd7dbdc7fb4a0d5356780d0de88c72042fcabd62
Eric Camellini <eric.camellini@buildo.io>
2017-05-25 10:44:38 +0200

    Step 2

--- src/app/components/Hello/Hello.js
+++ src/app/components/Hello/Hello.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import { intlMethods } from 'Basic';
 import { props, t } from 'tcomb-react';
-import connect from 'buildo-state/lib/connect';
 
 import './hello.scss';
 
 @intlMethods
-@connect({ formal: t.maybe(t.Boolean) })
 @props({
-  transition: t.Function,
-  formal: t.maybe(t.Boolean)
+  formal: t.Boolean,
+  toggle: t.Function
 })
 export default class Hello extends React.PureComponent {
-
-  toggle = () => {
-    this.props.transition({
-      formal: !this.props.formal
-    });
-  }
 
   render() {
     return (
       <div className='hello'>
         <h1>
-          <a onClick={this.toggle}>{this.props.formal ? 'Good Morning' : 'Hello'}</a>
+          <a onClick={this.props.toggle}>{this.props.formal ? 'Good Morning' : 'Hello'}</a>
         </h1>
       </div>
     );
