d358f4e80578f33ddee43d25d70bae5599177851
Eric Camellini <eric.camellini@buildo.io>
2017-05-25 10:53:29 +0200

    Step 3

--- src/app/components/Hello/Hello.js
+++ src/app/components/Hello/Hello.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { intlMethods } from 'Basic';
 import { props, t } from 'tcomb-react';
+import { skinnable } from 'revenge';
 
 import './hello.scss';
 
+function formalGreeting() {
+  const hours = new Date().getHours();
+  if (hours > 20) {
+    return 'Good Night';
+  } else if (hours > 13) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Morning';
+  }
+}
+
 @intlMethods
+@skinnable()
 @props({
   formal: t.Boolean,
   toggle: t.Function
 })
 export default class Hello extends React.PureComponent {
 
-  render() {
+  getLocals({ formal, toggle }) {
+    const greeting = formal ? formalGreeting() : 'Hello';
+
+    return { greeting, toggle };
+  }
+
+  template({ greeting, toggle }) {
     return (
       <div className='hello'>
         <h1>
-          <a onClick={this.props.toggle}>{this.props.formal ? 'Good Morning' : 'Hello'}</a>
+          <a onClick={toggle}>{greeting}</a>
         </h1>
       </div>
     );
