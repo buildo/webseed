9e0b135a46cd4f9244323e7abdc620f7df11e41f
Eric Camellini <eric.camellini@buildo.io>
2017-05-25 11:54:54 +0200

    Step 4

--- src/app/components/Hello/HelloContainer.js
+++ src/app/components/Hello/HelloContainer.js
@@ -4,10 +4,12 @@ import Hello from './Hello';
 
 export default container(Hello, {
   connect: { formal: t.maybe(t.Boolean) },
-  mapProps: ({ transition, formal = false }) => ({
+  queries: ['user'],
+  mapProps: ({ transition, formal = false, user }) => ({
     toggle: () => {
       transition({ formal: !formal });
     },
-    formal
+    formal,
+    user
   })
 });
