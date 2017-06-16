672dede2e8c4d2196bd48bfc306ccf3772154259
Eric Camellini <eric.camellini@buildo.io>
2017-06-16 11:30:58 +0200

    Step 3

--- src/app/components/Hello/Hello.tsx
+++ src/app/components/Hello/Hello.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { intlMethods } from 'Basic';
-
 import './hello.scss';
 
 type Props = {
@@ -8,16 +7,30 @@ type Props = {
   toggle: React.MouseEventHandler<HTMLAnchorElement>
 };
 
+function formalGreeting() {
+  const hours = new Date().getHours();
+  if (hours > 20) {
+    return 'Good Night';
+  } else if (hours > 13) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Morning';
+  }
+}
+
 @intlMethods
 export default class Hello extends React.PureComponent<Props> {
 
   formatMessage: (k: string) => string; // TODO: typo
 
   render() {
+    const { formal, toggle } = this.props;
+    const greeting = formal ? formalGreeting() : 'Hello';
+
     return (
       <div className='hello'>
         <h1>
-          <a onClick={this.props.toggle}>{this.props.formal ? 'Good Morning' : 'Hello'}</a>
+          <a onClick={toggle}>{greeting}</a>
         </h1>
       </div>
     );
